e47cc1cb47c2cac91b3f1de1dde86790d4fca3d2
xupan <18233566286@163.com>
2019-11-01 15:32:11 +0800

    chalk

--- ana/index.js
+++ ana/index.js
@@ -1,6 +1,9 @@
-var fs = require('fs');
-var path = require('path');//解析需要遍历的文件夹
-var filePath = path.resolve('./pages');
+const chalk = require('chalk');
+const log = console.log;
+
+let fs = require('fs');
+let path = require('path');//解析需要遍历的文件夹
+let filePath = path.resolve('./pages');
 //调用文件遍历方法
 fileDisplay(filePath);
 //文件遍历方法
@@ -12,7 +15,7 @@ function fileDisplay(filePath) {
         } else {
             //遍历读取到的文件列表
             let len = files.length;
-            console.log(`${filePath}目录的\n子文件夹个数：${len}\n文件个数${len * 4}`)
+            log(`${filePath}目录的\n子文件夹个数：${chalk.green(len)}\n文件个数${chalk.green(len * 4)}`)
         }
     });
 }
