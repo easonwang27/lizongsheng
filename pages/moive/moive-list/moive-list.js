2d3f22d9749bb0f63bc692e9c6f42c0d076f46de
xupan <18233566286@163.com>
2019-12-16 17:38:01 +0800

    细节

--- pages/moive/moive-list/moive-list.js
+++ pages/moive/moive-list/moive-list.js
@@ -22,7 +22,7 @@ Page({
         type: "动作",
         actor: ['道恩·强森', '凯伦·吉兰'],
         grade: 7.9,
-        name: "功夫熊猫",
+        name: "Sinister",
         img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575972845979&di=1733141893d6881981a150b421c66461&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fe21204e6a5460b4b8428e334bd8712ce11027c1faac6-wDFiPd_fw658"
       },
       {
@@ -37,7 +37,7 @@ Page({
         type: "动作",
         actor: ['道恩·强森', '凯伦·吉兰'],
         grade: 7.9,
-        name: "sinister",
+        name: "枪过境",
         img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576567660&di=9f2da50bf53d5cad34eae75983929476&imgtype=jpg&er=1&src=http%3A%2F%2Fimage13.m1905.cn%2Fuploadfile%2F2015%2F0325%2F20150325023802284318.jpg"
       },
       {
@@ -52,7 +52,7 @@ Page({
         type: "动作",
         actor: ['道恩·强森', '凯伦·吉兰'],
         grade: 7.9,
-        name: "敢死队",
+        name: "哈利波特",
         img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575972961306&di=9e776e1a515e051fb688c786d2d92487&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Ff1e26cd00d3f917b9cf81f8651e822a3b0638f789bdd-POCYRb_fw658"
       },
       {
@@ -67,7 +67,7 @@ Page({
         type: "动作",
         actor: ['道恩·强森', '凯伦·吉兰'],
         grade: 7.9,
-        name: "哈利波特",
+        name: "功夫熊猫",
         img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575972985921&di=55dc741c52956600b4450affc65f2841&imgtype=0&src=http%3A%2F%2Fwww.rjzxw.com%2Fdats%2Ftwjc%2F7%2F11875%2F11875-2.jpg"
       },
     ],
@@ -80,9 +80,6 @@ Page({
     }, {
       alias: "后天",
       date: "12月12日",
-    }, {
-      alias: "周五",
-      date: "12月13日",
     }],
     currentIndex: 0,
     curPoster: {},
